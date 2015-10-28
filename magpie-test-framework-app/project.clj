f189ce587c5f5e5478a5eb9e7bf680763453a038
Phoenix Zhang <pho.coderzhang@gmail.com>
2015-10-28 18:35:25 +0800

    fix

--- magpie-test-framework-app/project.clj
+++ magpie-test-framework-app/project.clj
@@ -1,11 +1,11 @@
-(defproject magpie-test-framework-app "0.1.1-SNAPSHOT"
+(defproject com.jd.bdp.magpie.magpie-eggs-clj/magpie-test-framework-app "1.1.7-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.1.4"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [com.jd.bdp.magpie/magpie-framework-clj "0.1.0-SNAPSHOT"]]
-  :main ^:skip-aot magpie-test-framework-app.core
+  :main ^:skip-aot com.jd.bdp.magpie.magpie-eggs-clj.magpie-test-framework-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- magpie-test-framework-app/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_test_framework_app/core.clj
+++ magpie-test-framework-app/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_test_framework_app/core.clj
@@ -1,19 +1,19 @@
-(ns magpie-test-framework-app.core
+(ns com.jd.bdp.magpie.magpie-eggs-clj.magpie-test-framework-app.core
   (:gen-class)
-  (:require [taoensso.timbre :as timbre]
+  (:require [clojure.tools.logging :as log]
             [com.jd.bdp.magpie.magpie-framework-clj.task-executor :as task-executor]))
 
 (defn run-fn [job-id]
-  (timbre/info (System/currentTimeMillis))
-  (timbre/info "run")
+  (log/info (System/currentTimeMillis))
+  (log/info "run")
   (Thread/sleep 3000))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
+  (log/info "Hello, World!")
   (try
     (task-executor/execute run-fn)
     (catch Throwable e
-      (prn e)
-      (prn "bye"))))
+      (log/error e)
+      (log/info "bye"))))
