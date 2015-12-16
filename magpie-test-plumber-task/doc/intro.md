40393f3895a6b61224daaf4bf0aa199eeeed7396
phoenix <pho.coderzhang@gmail.com>
2015-12-16 21:21:34 +0800

    mv project

--- magpie-test-plumber-task/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to magpie-test-framework-app
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- magpie-test-plumber-task/src/com/jd/bdp/magpie/magpie_eggs_clj/magpie_test_plumber_task/utils.clj
+++
@@ -1,9 +0,0 @@
-(ns com.jd.bdp.magpie.magpie-eggs-clj.magpie-test-plumber-task.utils)
-
-(defn check-albatross-alive?
-  [albatross-id])
-
-(defn check-task-valid?
-  [task-id]
-  (let [[prefix albatross-id job-id task-id] (clojure.string/split task-id #"*p*")]
-    ()))
